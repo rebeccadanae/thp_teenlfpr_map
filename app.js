54d00d34d7a391a4257513de0bf5c19d57d81268
rebeccadanae <rebeccadportman@gmail.com>
2020-09-23 09:52:06 -0400

    added THP data and set up create_map function with data retrieval inside of it

--- app.js
+++ app.js
@@ -56,44 +56,53 @@ d3.selection.prototype.moveToFront = function() {
 
     function tooltipHtml(n, d){	/* function to create html content string in tooltip div. */
   		return "<h4>"+n+"</h4><table>"+
-  			"<tr><td>Low</td><td>"+(d.low)+"</td></tr>"+
+  			"<tr><td>Region</td><td>"+(d.region)+"</td></tr>"+
   			"<tr><td>Average</td><td>"+(d.avg)+"</td></tr>"+
   			"<tr><td>High</td><td>"+(d.high)+"</td></tr>"+
   			"</table>";
   	}
-    var west = ["HI", "AK", "WA", "MT", "ID", "WY", "CO", "NM", "AZ", "UT", "NV", "CA", "UT", "OR"]
-    var midwest = ["ND", "MN", "WI", "MI", "OH", "IN", "IL", "MO", "IA", "SD", "NE", "KS"]
-    var northeast = ["ME", "NH", "VT", "NY", "PA", "NJ", "CT", "MA", "RI"]
-  	var sampleData ={};	/* Sample random data. */
-  	["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-  	"ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
-  	"MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
-  	"CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
-  	"WI", "MO", "AR", "OK", "KS", "LS", "VA"]
-  		.forEach(function(d, i){
-          var region=""
-          var num =""
-        if(west.includes(d)){
-          region = "west"
-          num = 25
-        }else if(midwest.includes(d)){
-          region = "midwest"
-          num = 50
-        }else if(northeast.includes(d)){
-          region = "northeast"
-          num = 75
-        }else{
-          region = "south"
-          num = 100
-        }
 
-  			sampleData[d]={region: region, color:d3.interpolate("#c4adce", "#5e2c70")(num/100)};
-  		});
-      console.log(sampleData)
-  	/* draw states on id #statesvg */
-  	uStates.draw("#statesvg", sampleData, tooltipHtml);
+      function create_map(){
+        d3.csv("map_summer.csv", function(data) {
+          console.log(data);
+        })
 
-  	d3.select(self.frameElement).style("height", "600px");
+        var west = ["HI", "AK", "WA", "MT", "ID", "WY", "CO", "NM", "AZ", "UT", "NV", "CA", "UT", "OR"]
+        var midwest = ["ND", "MN", "WI", "MI", "OH", "IN", "IL", "MO", "IA", "SD", "NE", "KS"]
+        var northeast = ["ME", "NH", "VT", "NY", "PA", "NJ", "CT", "MA", "RI"]
+        var sampleData ={};	/* Sample random data. */
+        ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+        "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
+        "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
+        "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
+        "WI", "MO", "AR", "OK", "KS", "LS", "VA"]
+          .forEach(function(d, i){
+              var region=""
+              var num =""
+            if(west.includes(d)){
+              region = "west"
+              num = 25
+            }else if(midwest.includes(d)){
+              region = "midwest"
+              num = 50
+            }else if(northeast.includes(d)){
+              region = "northeast"
+              num = 75
+            }else{
+              region = "south"
+              num = 100
+            }
+
+            sampleData[d]={region: region, color:d3.interpolate("#c4adce", "#5e2c70")(num/100)};
+          });
+          //console.log(sampleData)
+        /* draw states on id #statesvg */
+        uStates.draw("#statesvg", sampleData, tooltipHtml);
+
+        d3.select(self.frameElement).style("height", "600px");
+      }
+    create_map()
+
 
 
     /*
